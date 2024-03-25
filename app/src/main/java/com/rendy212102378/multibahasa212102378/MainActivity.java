aab21c49f93b515fc8daa67b0864fd1d3f4b5141
Renn <119943753+REnnnnxde@users.noreply.github.com>
2024-03-25 10:11:51 +0700

    Initial commit

---
+++ app/src/main/java/com/rendy212102378/multibahasa212102378/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rendy212102378.multibahasa212102378;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Multi Bahasa212102378"
+include(":app")
+ 
